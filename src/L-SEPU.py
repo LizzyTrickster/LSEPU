01f8fe442402796c73c73d90c92eccf369990c10
Lizzy Trickster <LizzyTrickster@users.noreply.github.com>
2019-06-05 16:57:49 +0100

    More updates! It now download the files and checks the md5 hashes

--- src/L-SEPU.py
+++ src/L-SEPU.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright
-
+import hashlib
 import os
 import requests
 import PySimpleGUI as sg
@@ -13,12 +13,13 @@ for envvar in ["INST_NAME", "INST_ID", "INST_DIR", "INST_MC_DIR", "INST_JAVA", "
         break
 
 print(f"Running in MultiMC? {IsMMC}")
+session = requests.Session()
 
 WindowLayout = [
     [sg.Text("Lizzy's updater", key='test')],
     [sg.Text("Getting pack metadata:"), sg.ProgressBar(1, key="pb-mod-index")],
     [sg.Text("Downloading: "), sg.ProgressBar(1, key="pb-mod-download")],
-    [sg.Output(size=(100, 4))]
+    [sg.Output(size=(100, 6))]
 ]
 
 mainWindow = sg.Window("Lizzy's Solder-Enabled Pack Updater", WindowLayout).Finalize()
@@ -29,33 +30,59 @@ solderInstance = "https://solder.theender.net/"
 solderPackName = "techycraft-30"
 
 modpack_meta = dict()
+moddata = dict()
 
 # Look up pack details
-with requests.get(f"{solderInstance}/api/modpack/{solderPackName}") as resp:
+with session.get(f"{solderInstance}/api/modpack/{solderPackName}") as resp:
     try:
         modpack_meta['recommended'] = resp.json()["recommended"]
     except Exception:
-        pass
+        raise
 
 print(f"Recommended pack version: {modpack_meta['recommended']}")
-event, value = mainWindow.Read()
-with requests.get(f"{solderInstance}/api/modpack/{solderPackName}/{modpack_meta['recommended']}") as resp:
+event, value = mainWindow.Read(timeout=0)
+with session.get(f"{solderInstance}/api/modpack/{solderPackName}/{modpack_meta['recommended']}") as resp:
     try:
         data = resp.json()
     except:
-        pass
+        raise
     else:
         modpack_meta["mods"] = data["mods"]
 print("Processing modlist....")
 index_bar.UpdateBar(0, max=len(modpack_meta["mods"]))
-event, value = mainWindow.Read()
+event, value = mainWindow.Read(timeout=0)
 
 # Process mod metadata
 for mod in modpack_meta["mods"]:
+    with session.get(f"{solderInstance}/api/mod/{mod['name']}") as resp:
+        data = resp.json()
+    #if data["description"] is not None and "#clientonly" in data['description']:
+    #    client_only = True
+    moddata[mod['name']] = dict(version=mod['version'], md5=mod['md5'], url=mod['url'])
+    index_bar.UpdateBar(modpack_meta['mods'].index(mod))
+    print(f"Processing {mod['name']}")
+    event, value = mainWindow.Read(timeout=0)
 
-
-# with requests.get(f"{solderInstance}/api/mod/")
+CACHE_DIR = "X:\\L-SEPU\\crap"
 
 # Download the mods
+ii = 1
+for mod in moddata:
+    m = hashlib.md5()
+    print(f"Downloading mod: {mod}")
+    with session.get(f"{moddata[mod]['url']}", stream=True) as resp:
+        try:
+            with open(os.path.join(CACHE_DIR, f"{mod}-{moddata[mod]['version']}.zip"), "wb") as f:
+                for chunk in resp.iter_content(chunk_size=1024):
+                    # Chunked download so that the window can be "refreshed" when downloading large files
+                    if chunk:
+                        f.write(chunk)
+                        m.update(chunk)
+                    event, value = mainWindow.Read(timeout=0)
+        except:
+            raise
+    print(f"Do the MD5 Hashes match? {m.hexdigest() == moddata[mod]['md5']}")
+    mod_bar.UpdateBar(ii, max=len(moddata))
+    ii = ii + 1
 
 # Extract zips
